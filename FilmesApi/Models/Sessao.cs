e13850724f9e2f551feec8988e7bdd9d4ab13945
matheus.silva <tecmatheusg@hotmail.com>
2022-10-26 10:34:21 -0300

    Horário de início adicionado ao recuperar informação sobre sessão'

--- FilmesApi/Models/Sessao.cs
+++ FilmesApi/Models/Sessao.cs
@@ -1,6 +1,7 @@
 ﻿using FilmesAPI.Models;
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace FilmesApi.Models
 {
@@ -9,10 +10,13 @@ namespace FilmesApi.Models
         [Key]
         [Required]
         public int Id { get; set; }
+        [JsonIgnore]
         public virtual Filme Filme { get; set; }
         public int FilmeId { get; set; }
+        [JsonIgnore]
         public virtual Cinema Cinema { get; set; }
         public int CinemaId { get; set; }
-        public DateTime HorarioDeEncerramento { get; set; }
+        [JsonIgnore]
+        public virtual DateTime HorarioDeEncerramento { get; set; }
     }
 }
